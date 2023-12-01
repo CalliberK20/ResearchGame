1f38d3613d6a0396ee55bd565c1dcbcc4549f93d
CalliberK20 <angrybirds.special2001@gmail.com>
2023-12-02 01:13:55 +0800

    Door Entrance
    New update
    -Added a entry price here, it will show if the player has the enough currency, if he does and he chooses to buy it, then he can procceed.
    
    Fix
    -Level to Difficulty menus
    -Darken the quit background

--- Assets/Door.cs
+++ Assets/Door.cs
@@ -1,27 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    public Door parentDoor;
+    [Space]
     public float radius = 1.5f;
+    public float entryPass = 30f;
+    [Space]
+    public TextMeshPro textPrice;
+    public GameObject doorSpriteObj;
+    public GameObject barrierCollider;
+    [Space]
     public LayerMask layersToOpen;
-    public Collider2D[] collider;
-    private GameObject doorSpriteObj;
-    // Start is called before the first frame update
-    void Start()
+    public List<Collider2D> collider = new List<Collider2D>();
+
+    private bool isBought = false;
+
+    private void Start()
     {
-        doorSpriteObj = transform.GetChild(0).gameObject;
+        if (parentDoor != null)
+        {
+            textPrice.gameObject.SetActive(false);
+            barrierCollider.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        collider = Physics2D.OverlapCircleAll(transform.position, radius, layersToOpen);
+        collider = Physics2D.OverlapCircleAll(transform.position, radius, layersToOpen).ToList();
 
-        if(collider.Length > 0)
-            doorSpriteObj.SetActive(false);
-        else doorSpriteObj.SetActive(true);
+        if (collider.Count > 0)
+        {
+            if(parentDoor != null)
+            {
+                doorSpriteObj.SetActive(false);
+            }
+            else if (!collider[0].CompareTag("Player") || isBought)
+                doorSpriteObj.SetActive(false);
+            else if (!isBought)
+            {
+                textPrice.gameObject.SetActive(true);
+                textPrice.text = "Unlock: " + entryPass;
+            }
+        }
+        else
+        {
+            textPrice.gameObject.SetActive(false);
+            doorSpriteObj.SetActive(true);
+        }
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if (!isBought && CashManager.instance.cash >= entryPass)
+            {
+                isBought = true;
+                textPrice.gameObject.SetActive(false);
+                barrierCollider.SetActive(false);
+            }
+        }
     }
 
     private void OnDrawGizmos()
